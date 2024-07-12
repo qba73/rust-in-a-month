4470537b414566419036b5c41503c62f7e9b3286
Jakub Jarosz <jakub@jarosz.dev>
2024-07-12 02:11:23 +0100

    Declaring vars

---
+++ chapter-01/declaring-vars/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    let google_base = "google";
+    let yahoo_base = "yahoo";
+
+    println!("The url is www.{google_base}.com");
+    println!("The url is www.{}.com", yahoo_base);
+}
